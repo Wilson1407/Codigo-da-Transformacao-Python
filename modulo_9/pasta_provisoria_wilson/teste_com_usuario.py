ed744b8ff47d0e7331090c304dea7938e7dae662
Wilson <wilson140207@gmail.com>
2026-05-20 16:23:06 -0300

    Add files via upload

--- modulo_9/pasta_provisoria_wilson/teste_com_usuario.py
+++ modulo_9/pasta_provisoria_wilson/teste_com_usuario.py
@@ -1,18 +1,29 @@
 class Celular:
     def __init__(self, marca, modelo):
-        self.marca, self.modelo = marca, modelo
+        self.marca = marca
+        self.modelo = modelo
         self.bateria = 100
 
     def fazer_chamada(self, custo_bateria):
         print(f"\n--- chamada no {self.modelo} ---")
         try:
-            self.bateria -= custo_bateria
+            custo = int(custo_bateria)
+
+            if custo > self.bateria:
+                print("Erro: Bateria Insuficiente!")
+            else:
+                 self.bateria -= custo
+                 print(f"Sucesso! Bateria atual: {self.bateria}%")
+
         except TypeError:
             print("Erro: O custo da bateria deve ser um número!")
-        else:
-            print(f"Sucesso! bateria atual: {self.bateria}%")
+        except ValueError:
+            print("Erro: Digite um número inteiro válido!")
         finally:
-            print("Sistema finalizado.")
+            print("Sistema encerrada.")
 
 meu_celular = Celular("Samsung", "S24")
 meu_celular.fazer_chamada("muito")
+meu_celular.fazer_chamada(30)
+meu_celular.fazer_chamada(80)
+print(f"\nbateria final: {meu_celular.bateria}%")
